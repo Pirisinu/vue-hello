7f208397a7f3e123b66afabe6284f37578316e9c
Pirisinu <palazzialessandro2@gmail.com>
2023-09-27 16:11:07 +0200

    [ADD]img+ button effect

--- js/script.js
+++ js/script.js
@@ -3,7 +3,13 @@ const { createApp } = Vue;
 createApp({
   data(){
     return{
-      messaggio: 'Ciao da Vue'
-    }
+      messaggio: 'Ciao da Vue',
+      image: 'https://picsum.photos/400',
+      effectClass: ['opacity', 'scale', 'translate'],
+      effectClass2: 'opacity'
+    };
+
+
+    
   }
 }).mount('#app');
